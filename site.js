e4632b4f04658cb68b92434c4b70b16310ef1cce
Schuh <500190471@fvtc.edu>
2024-11-23 16:27:32 -0600

    Back to Original

--- site.js
+++ site.js
@@ -3,11 +3,7 @@ const p = document.querySelector('p')
 const input = document.querySelector('input')
 const button = document.querySelector('button')
 
-//button.addEventListener('click', () => p.textContent = input.value)
-
-let currentImage = 0
-button.addEventListener('click', () => currentImage + 1)
-console.log(currentImage)
+button.addEventListener('click', () => p.textContent = input.value)
 
 input.addEventListener('keydown', event => {
     if (event.code === 'Space') event.preventDefault()
